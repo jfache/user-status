45ba19a7140a90788e09a81fc9687eb7638e78d7
Jérémy Fache <jfache@gmail.com>
2015-03-31 16:43:52 -0700

    updated initial sql to add database if missing, updated sample config accordingly

--- inc/config-sample.php
+++ inc/config-sample.php
@@ -5,4 +5,4 @@ define('BASE_URL', 'http://localhost/project');
 define('DB_HOST', 'localhost');
 define('DB_USER', '');
 define('DB_PASSWORD', '');
-define('DB_NAME', '');
+define('DB_NAME', 'demo_user_status');
--- sql/demo_user_status.sql
+++ sql/demo_user_status.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.10.1
+-- version 4.2.5
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 19, 2012 at 06:55 PM
--- Server version: 5.5.28
--- PHP Version: 5.4.6-1ubuntu1
+-- Generation Time: Mar 31, 2015 at 04:41 PM
+-- Server version: 5.5.41-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.7
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,8 +17,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `clio_user_status`
+-- Database: `demo_user_status`
 --
+CREATE DATABASE IF NOT EXISTS `demo_user_status` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `demo_user_status`;
 
 -- --------------------------------------------------------
 
@@ -27,13 +29,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `username` varchar(255) CHARACTER SET latin1 NOT NULL,
   `password` varchar(255) CHARACTER SET latin1 NOT NULL,
   `date_joined` datetime NOT NULL,
-  `last_login` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
+  `last_login` datetime NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=7 ;
 
 -- --------------------------------------------------------
@@ -43,14 +43,42 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 CREATE TABLE IF NOT EXISTS `user_statuses` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `status` varchar(255) COLLATE utf8_bin NOT NULL,
-  `date_posted` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`)
+  `date_posted` datetime NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=10 ;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `user_statuses`
+--
+ALTER TABLE `user_statuses`
+ ADD PRIMARY KEY (`id`), ADD KEY `user_id` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `user_statuses`
+--
+ALTER TABLE `user_statuses`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
